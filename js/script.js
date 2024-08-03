d91f97fdd2f70659045a92fd8e9fc64f29fad2db
BrithanyHernandez <brithanyrub@gmail.com>
2024-08-02 19:07:50 -0600

    animacion con js

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let app = document.getElementById('app');
+
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+// Usar el punto es para llamar a una propiedad o a una acción del objeto.
+// Acciones se distinguen por los paréntesis ()
+typewriter
+  .pauseFor(2800)
+  .typeString('Brithany Hernandez')
+  .pauseFor(300)
+  .deleteAll()
+  .typeString('Desarrolladora Frontend JR')
+  .pauseFor(1000)
+  .start();
+
+
+let frase = document.getElementById('frase');
+
+let typewriterFrase = new Typewriter(frase, {
+loop: true,
+delay: 75,
+});
+  
+  // Usar el punto es para llamar a una propiedad o a una acción del objeto.
+  // Acciones se distinguen por los paréntesis ()
+  typewriterFrase
+    .pauseFor(2500)
+    .typeString('...Por lo tanto, lo que debemos hacer es esforzarnos y perseverar y NUNCA RENDIRNOS.')
+    .pauseFor(300)
+    .deleteAll()
+    .typeString('Dalai Lama')
+    .pauseFor(1000)
+    .start();
